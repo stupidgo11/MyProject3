e181723a780d0b0f0f1f90bbca59f32ac92fd8a8
Natthaphol <zerozkiller@gmail.com>
2023-03-22 15:48:06 +0700

    Revert "add hello"
    This reverts commit e9372965247e4c2e86fa10b6e0910c4c081bca30.

--- Myproject3.java
+++ Myproject3.java
@@ -5,7 +5,7 @@ public class Myproject3 {
 
     public static void main(String[] args) {
         System.out.println("Hello Natthaphol");
-        System.out.println("Hello World");
+        System.out.println("Hello Yada");
     }
     
 }
